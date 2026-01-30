5d94d16a3a17f6de97570cece0f1a372ab9e7aa7
Benjamin Philipose <philiposebenjamin@gmail.com>
2026-01-30 00:01:00 -0800

    linking fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bphilipose-diagnostics', // For bphilipose.github.io, this must be '/'
+  base: '/bphilipose-diagnostics/', // THIS MUST MATCH THE REPO NAME
 })
