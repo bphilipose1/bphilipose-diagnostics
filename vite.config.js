dabab601f37aabd89622a853e31114c43bfacea9
Benjamin Philipose <philiposebenjamin@gmail.com>
2026-01-29 23:34:52 -0800

    Clean repo files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bphilipose.github.io', // For bphilipose.github.io, this must be '/'
+  base: '/bphilipose-diagnostics', // For bphilipose.github.io, this must be '/'
 })
